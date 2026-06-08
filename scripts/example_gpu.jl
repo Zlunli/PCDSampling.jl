9f74e1189c189147445d51ef66c782131f35d325
Zlunli <zhilun.li@keemail.me>
2026-06-08 16:20:06 +0200

    Update example_gpu.jl

--- scripts/example_gpu.jl
+++ scripts/example_gpu.jl
@@ -4,9 +4,17 @@ using LinearAlgebra
 using CairoMakie
 using CUDA
 
-function sample_gaussian_gpu(;N=1000, L=128, use_local=false)
+function sample_gaussian_gpu(; N=1000, P=200, L=128, use_local=false)
     dist = MixtureModel([MvNormal([0.0, 0.0], I(2))])
-    @time X, iters = draw_samples_gpu(dist, N, uniform_directions_2d(200); use_local, max_iters=100, N_lut=L, verbose=true)
+    @time X, iters = draw_samples_gpu(
+        dist,
+        N,
+        uniform_directions_2d(P);
+        use_local,
+        max_iters=100,
+        N_lut=L,
+        verbose=true,
+    )
 
     f = Figure()
     ax = Axis(f[1, 1], aspect=DataAspect())
@@ -14,9 +22,17 @@ function sample_gaussian_gpu(;N=1000, L=128, use_local=false)
     display(f)
 end
 
-function sample_mixture_gpu(;C=10, N=1000, L=128, use_local=false)
+function sample_mixture_gpu(; C=10, N=1000, P=200, L=128, use_local=false)
     dist = MixtureModel([MvNormal(randn(2), Diagonal(randn(2).^2)) for _ in 1:C])
-    @time X, iters = draw_samples_gpu(dist, N, uniform_directions_2d(200); use_local, max_iters=100, N_lut=L, verbose=true)
+    @time X, iters = draw_samples_gpu(
+        dist,
+        N,
+        uniform_directions_2d(P);
+        use_local,
+        max_iters=100,
+        N_lut=L,
+        verbose=true,
+    )
 
     f = Figure()
     ax = Axis(f[1, 1], aspect=DataAspect())
